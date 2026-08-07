84c4a472a302cc4bda63eb3d5baed31305d00062
Ivan Giraldez moral <ivangiraldezmoral@MacBook-Air-de-Ivan.local>
2026-08-07 21:59:44 +0200

    Actualiza sección inmobiliarias

---
+++ backup-data/data.ts
@@ -0,0 +1,19 @@
+export const project = {
+  title:"Pobla Lledó Session 2025",
+
+  location:"La Pobla de Mafumet",
+
+  description:"Fiestas del Lledó con Mondj.",
+
+  heroVideo:"/assets/videos/eventos/pobla-lledo-session-2025/video.mp4",
+
+  artists:[
+    "DJ Salmer",
+    "DJ Miki",
+    "Mondj"
+  ],
+
+  services:["Fotografía profesional","Vídeo profesional"],
+
+  images:Array.from({length:12},(_,i)=>`/assets/images/eventos/pobla-lledo-session-2025/${i+1}.jpg`)
+};
